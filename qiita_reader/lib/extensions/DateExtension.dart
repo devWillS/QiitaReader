421ad4d21eb81e43079ff6deb691221d54af0dad
Seidi Nakamura <seidi.nakamura@stv-tech.co.jp>
2020-03-08 16:22:03 +0900

    不要なprint文の削除

--- qiita_reader/lib/extensions/DateExtension.dart
+++ qiita_reader/lib/extensions/DateExtension.dart
@@ -5,14 +5,10 @@ extension DateExtension on String {
   get formatDate {
     if (this != null) {
       initializeDateFormatting("ja_JP");
-      print(this);
 
       DateTime datetime = DateTime.parse(this).toLocal(); // StringからDate
-      print(datetime);
-      print(datetime.timeZoneName);
       var formatter = new DateFormat('yyyy年MM月dd日に投稿', "ja_JP");
       var formatted = formatter.format(datetime); // DateからString
-      print(formatted);
       return formatted;
     } else {
       return null;
